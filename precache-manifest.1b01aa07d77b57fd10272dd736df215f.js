bc2fa24006d6e4a38c39c3423b4b8a88a2be82e0
Kirill <work.kirillabramov@gmail.com>
2019-03-10 23:24:29 +0200

    Updates

---
+++ precache-manifest.1b01aa07d77b57fd10272dd736df215f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c71ba4c6707e371f7554",
+    "url": "/Rijksmuseum-app/static/css/main.51219ac6.chunk.css"
+  },
+  {
+    "revision": "c71ba4c6707e371f7554",
+    "url": "/Rijksmuseum-app/static/js/main.c71ba4c6.chunk.js"
+  },
+  {
+    "revision": "71f5071beb7d2fd6f940",
+    "url": "/Rijksmuseum-app/static/js/1.71f5071b.chunk.js"
+  },
+  {
+    "revision": "2cdf335ed636afd113bd",
+    "url": "/Rijksmuseum-app/static/js/runtime~main.2cdf335e.js"
+  },
+  {
+    "revision": "b24fc4f94f3d1d517e1a5412cddb11e5",
+    "url": "/Rijksmuseum-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Rijksmuseum-app/precache-manifest.1b01aa07d77b57fd10272dd736df215f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Rijksmuseum-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
